e819e9f31efd3eea62cc32380a43b0b2ff4202fe
Atai Ambus <atayambus@gmail.com>
2022-01-10 17:07:19 +0200

    feat(tests): add autodetection of tested columns
    - Test class added a test_row method used for the testing of each row. Instead of just returning a boolean, it returns a tuple of boolean and List[str], which is the list of columns tested in the row. Note that this is returned per row rather than for the test itself, since a test may check different cells based on specific cells in a row. 
    
    - The tested columns detection is done with sys.settrace. First, before preforming the test o the row, a tracing funciotn is set which checks for calls of Series.__getitem__ where the series is the row being tested — that is, calls where the test accesses cells in the row. The tracing funciton gathers the columns used on each access. The tracing funciton runs along the test of the row and is then reset to its previous state. Then the test result is returned alongside the gathered columns. 
    
    - This method is still rather crude, since it will autodetect any column accessed during the test, which may not really be intended as an actula test of these columns, and may just be use them to compute the normal value of a different column (e.g normal weight/height of men vs. women)
    
    - For this reason, though it is definitely not a complete solution, In the future I plan to add an optional columns param to Test to override the autodetection behaviour.
    
    
    - DBTests no longer keeps a dictionary of column-to-tests. This is because the columns tested are only computed alongside the tests themselved and are not known ahead of time. Instead, a simple list of tests is used.
    
    - on run(), DBTest iterates over its test list and keeps a set of all columns tested in each Test ran, and keeps a simple list of test results, which is then iterated over to create the Dictionary of column-to-results, where results may be duplicated across columns. Then it runs as it did previously.
    
    
    - Also some formatting changes, and in the crude test which runs as main, use functools.partial instead of eval

--- tests.py
+++ tests.py
@@ -1,18 +1,17 @@
 from __future__ import annotations
 
 import functools
-from typing import List, Callable, Dict
+import sys
+from typing import List, Callable, Dict, Tuple, Set
 
 import pandas
 from pandas import DataFrame, Series
 
-import columndetection
 import unittest
 
 
 class Test:
-    def __init__(self, column: str, test: Callable[[Series], bool], name: str = None):
-        self.column = column
+    def __init__(self, test: Callable[[Series], bool], name: str = None):
         self.test = test
 
         if name is None:
@@ -20,32 +19,60 @@ class Test:
         else:
             self.name = name
 
+    '''
+    Returns the tuple of the test result and the columns the test accessed
+    '''
+
+    def test_row(self, row: Series) -> Tuple[bool, Set[str]]:
+        accessed_columns = set()
+
+        def add_accessed_columns(frame, event, arg):
+            if event == 'call' \
+                    and frame.f_code == Series.__getitem__.__code__ \
+                    and frame.f_locals['self'].equals(row):
+                accessed_columns.add(frame.f_locals['key'])
+
+        old = sys.gettrace()
+        sys.settrace(add_accessed_columns)
+
+        try:
+            test_result = self.test(row)
+        finally:
+            sys.settrace(old)
+
+        return test_result, accessed_columns
+
     def run(self, rows: List[Series]) -> TestResult:
         success = True
+        columns_tested = set()
         rows_of_failure = []
         for row in rows:
-            if not self.test(row):
+            row_success, columns_tested_in_row = self.test_row(row)
+            if not row_success:
                 success = False
                 rows_of_failure.append(row)
-        return TestResult(self, success, rows_of_failure)
+            columns_tested = columns_tested.union(columns_tested_in_row)
+        return TestResult(self, columns_tested, success, rows_of_failure)
 
 
 class TestResult:
-    def __init__(self, from_test: Test, success=True, rows_of_failure=None):
-        self.from_test = from_test
+    def __init__(self, origin_test: Test, columns_tested: Set[str], success=True, rows_of_failure=None):
+        self.from_test = origin_test
+        self.columns_tested = columns_tested
         self.success = success
         self.rows_of_failure = [] if self.success else rows_of_failure
 
 
 class ResultSet:
-    def __init__(self, dataframe: DataFrame, column: str):
+    def __init__(self, dataframe: DataFrame, column: str, index: int):
         self.dataframe: DataFrame = dataframe
         self.column: str = column
         self.results: List[TestResult] = []
+        self.index = index
 
     def add_result(self, result: TestResult):
-        if result.from_test.column != self.column:
-            raise ValueError('Incompatible columns: added test result for column ' + result.from_test.column
+        if self.column not in result.columns_tested:
+            raise ValueError('Incompatible columns: added test result for columns ' + str(result.columns_tested)
                              + ' for a ' + self.column + ' ResultSet!')
 
         self.results.append(result)
@@ -54,44 +81,44 @@ class ResultSet:
 class DBTests:
     def __init__(self, df: DataFrame):
         self.dataframe: DataFrame = df
-        self.tests: Dict[str, List[Test]] = {}
-        for column in df:
-            self.tests[column] = []
+        self.tests: List[Test] = []
+        self.columns_tested = set()
 
-    def add_test(self, column: str, test_func: Callable[[Series], bool], name: str = None):
+    def add_test(self, test_func: Callable[[Series], bool], name: str = None):
         try:
-            test = Test(column, test_func, name)
-            self.tests[test.column].append(test)
-            return True
+            test = Test(test_func, name)
+            self.tests.append(test)
         except KeyError:
             raise ValueError('Tried to register a test for a column that does not exist in the dataframe!')
 
     def run(self, show_valid_cols=False, show_untested=False, stub=False):
-
         num_rows, num_cols = self.dataframe.shape
         # Tests is a dict by column, so its size is the amount of columns that have tests.
-        cols_checked = [column for column, column_tests in self.tests.items() if len(column_tests) > 0]
-        num_cols_checked = len(cols_checked)
         # Initialize an empty test_result list for each column tested. There will be one result object
         # For each existing test.
-        results_by_column: Dict[str, List[TestResult]] = {column: [] for column in self.tests.keys()}
+        results_by_column: Dict[str, List[TestResult]] = {column: [] for column in self.dataframe}
 
         rows = [row for i, row in self.dataframe.iterrows()]
-
         column_validity = {}
-        for column in results_by_column.keys():
-            for test in self.tests[column]:
-                results_by_column[column].append(test.run(rows))  # Run it on the cell and
 
+        for test in self.tests:
+            test_results = test.run(rows)
+            for column in test_results.columns_tested:
+                results_by_column[column].append(test_results)
+
+        cols_checked = [column for column, result_list in results_by_column.items() if len(result_list) > 0]
+        num_cols_checked = len(cols_checked)
+
+        for column in cols_checked:
             column_validity[column] = all(result.success for result in results_by_column[column])
 
         valid_cols = sum(1 for column, validity in column_validity.items() if validity)
         print(f'Columns Tested: {num_cols_checked}/{num_cols} ({round(num_cols_checked / num_cols * 100)}%).')
-        print(f'Columns valid: {valid_cols}/{num_cols} ({round(valid_cols/num_cols*100, 2)}%).')
+        print(f'Columns valid: {valid_cols}/{num_cols} ({round(valid_cols / num_cols * 100, 2)}%).')
 
         if not stub:
             print()
-            for i, column in enumerate(self.tests.keys(), 1):
+            for i, column in enumerate(self.dataframe.columns, 1):
                 if (column in cols_checked or show_untested) and (not column_validity[column] or show_valid_cols):
                     print(f'--- Column {i}: {column} ---')
                     for j, result in enumerate(results_by_column[column], 1):
@@ -117,20 +144,13 @@ if __name__ == '__main__':
     bool_cols = ['Private']
 
     for bool_col in bool_cols:
-        tests.add_test(bool_col, functools.partial(unittest.boolean_test, bool_col), bool_col + ' Yes/No compliance')
+        tests.add_test(functools.partial(unittest.boolean_test, bool_col), bool_col + ' Yes/No compliance')
 
     numerical_cols = ['Apps', 'Accept', 'Enroll', 'Top10perc', 'Top25perc', 'F.Undergrad', 'P.Undergrad', 'Outstate',
                       'Room.Board', 'Books', 'Personal', 'PhD', 'Terminal', 'perc.alumni', 'Expend', 'Grad.Rate']
     for num_col in numerical_cols:
-        tests.add_test(num_col, functools.partial(unittest.integer_test, num_col), num_col + ' Integer Compliance')
+        tests.add_test(functools.partial(unittest.integer_test, num_col), num_col + ' Integer Compliance')
 
-    tests.add_test('Top10perc', unittest.tenpercent_test)
+    tests.add_test(unittest.tenpercent_test)
 
-
-    def my_test():
-        unittest.tenpercent_test(db.iloc[1])
-
-
-    # columndetection.get_affecting_columns(unittest.tenpercent_test)
-    # print(columndetection.test_call(my_test, db.__getitem__))
     tests.run()
