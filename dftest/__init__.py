8f73aac023e18d190b1e71a1d187310991554ba4
Atai Ambus <atayambus@gmail.com>
2022-02-13 13:39:50 +0200

    doc(setup): bump up version

--- dftest/__init__.py
+++ dftest/__init__.py
@@ -2,4 +2,4 @@ from dftest.DBTests import DBTests
 import dftest.tests as tests
 from dftest.options import options
 
-__version__ = '0.1.1'
+__version__ = '0.2.1'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='dftest',
     packages=['dftest'],
-    version='0.1.1',
+    version='0.2.1',
     scripts=['bin/dftest'],
     description='A library for testing and analyzing data integrity.',
     author='Atai Ambus',
