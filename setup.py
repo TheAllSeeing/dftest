da718c24c3f842d6f0faa5939e06a1a1b8bbb84f
Atai Ambus <atayambus@gmail.com>
2022-02-16 09:10:40 +0200

    feat(setup): specify dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
+import os
+
 from setuptools import setup
+
+with open('requirements.txt') as req_file:
+    deps = req_file.readlines()
+
+if 'COLAB_GPU' not in os.environ:
+    deps.append('pandasgui')
 
 setup(
     name='dftest',
     packages=['dftest'],
     version='0.2.1',
     scripts=['bin/dftest'],
+    install_requires=deps,
     description='A library for testing and analyzing data integrity.',
     author='Atai Ambus',
-    license='MIT',
+    license='MIT'
 )
